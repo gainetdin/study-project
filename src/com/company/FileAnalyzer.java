36bd16eeb89296eafd62a95743cc2e6b76998cbc
gainetdin <gainetdin@ya.ru>
2021-10-19 00:16:39 +0300

    added FileReader implementation example

--- src/com/company/FileAnalyzer.java
+++ src/com/company/FileAnalyzer.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.io.*;
 import java.util.logging.Logger;
 
 public class FileAnalyzer {
@@ -7,5 +8,15 @@ public class FileAnalyzer {
     public static void main(String[] args) {
         Logger logger = Logger.getLogger(FileAnalyzer.class.getName());
         logger.info("Program started");
+        File exampleFile = new File("/home/gainetdin/IdeaProjects/FileWordCounter/testFiles/fnk.txt");
+        try (BufferedReader reader = new BufferedReader(new FileReader(exampleFile))) {
+            int fileSymbol = reader.read();
+            while (fileSymbol != -1) {
+                System.out.print((char) fileSymbol);
+                fileSymbol = reader.read();
+            }
+        } catch (IOException e) {
+            logger.warning( e.getMessage() );
+        }
     }
 }
