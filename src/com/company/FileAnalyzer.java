71ac7ed4ad44a3782a637b4c7ba4eba072888b33
gainetdin <gainetdin@ya.ru>
2021-10-22 00:11:02 +0300

    added Scanner implementation and word count via cycle

--- src/com/company/FileAnalyzer.java
+++ src/com/company/FileAnalyzer.java
@@ -1,22 +1,32 @@
 package com.company;
 
 import java.io.*;
+import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class FileAnalyzer {
 
     public static void main(String[] args) {
         Logger logger = Logger.getLogger(FileAnalyzer.class.getName());
-        logger.info("Program started");
-        File exampleFile = new File("/home/gainetdin/IdeaProjects/FileWordCounter/testFiles/fnk.txt");
-        try (BufferedReader reader = new BufferedReader(new FileReader(exampleFile))) {
-            int fileSymbol = reader.read();
-            while (fileSymbol != -1) {
-                System.out.print((char) fileSymbol);
-                fileSymbol = reader.read();
+
+        try (Scanner scanner = new Scanner(new File(args[0]))) {
+
+            String matchingWord = args[1];
+            int countMatching = 0;
+            scanner.useDelimiter("[^a-zA-Zа-яА-Я0-9]+"); //regex to ignore punctuation marks
+
+            while (scanner.hasNext()) {
+                if (scanner.next().equalsIgnoreCase(matchingWord)) {
+                    countMatching++;
+                }
             }
+            System.out.println("Number of \"" + args[1] + "\" words found: " + countMatching);
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            logger.warning("Specify 2 arguments: \"file path\" \"word to count\"");
+
         } catch (IOException e) {
-            logger.warning( e.getMessage() );
+            logger.warning(e.getMessage());
         }
     }
 }
